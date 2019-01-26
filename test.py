9db37a3ba2d9d0eb5816682bd4b90ffe5e129994
Misael <aguayo.misael@gmail.com>
2019-01-26 15:37:59 -0600

    test2

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import smartcar
-
+import webbrowser
 #smartcar.AuthClient(client_id, client_secret, redirect_uri, scope, test_mode)
 client = smartcar.AuthClient("90844701-84f0-4845-bb9f-6c060ce22216", "c489c726-b7dc-4f77-bcdf-a4584ac9cdb9", "http://localhost:8000/callback", scope=None, test_mode=False)
 client.get_auth_url(force=True)
+
+print(client.get_auth_url())
 #helloooo
 #ok
